0645bb8fde4b558d912aae99370600dd0d1e5759
Fajarky <fajarstok15@gmail.com>
2024-07-15 14:29:01 +0700

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,12 @@
+import os, requests
+from SFInsta import Insta as Gram
+
+if __name__ == '__main__':
+	try:os.mkdir('/sdcard/Ress')
+	except:pass
+	try:os.mkdir('Data')
+	except:pass
+	try:
+		Gram.License()
+	except requests.exceptions.ConnectionError:
+		print('Connection Close')
